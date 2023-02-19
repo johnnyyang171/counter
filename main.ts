03b92788355c04636fbfe682730a88f142ad34ee
johnnyyang171 <116053159+johnnyyang171@users.noreply.github.com>
2023-02-19 10:34:41 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    basic.showNumber(count)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(count)
+})
+input.onButtonPressed(Button.B, function () {
+    count += 1
+    basic.showNumber(count)
+})
+let count = 0
+basic.showNumber(0)
+let counter = 0
+basic.forever(function () {
+	
+})
